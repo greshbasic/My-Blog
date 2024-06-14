ea1a12a6b8574c63d865163bec21b7bc30a9c152
Gresh Basic <basic.gresham@gmail.com>
2024-06-14 11:24:30 -0500

    styling, db, and section 6

--- my_site/blog/models.py
+++ my_site/blog/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+
+class Tag(models.Model):
+    caption = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.caption
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField()
+
+    def full_name(self):
+        return f'{self.first_name} {self.last_name}'
+
+    def __str__(self):
+        return self.full_name()
+    
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    excerpt = models.CharField(max_length=200)
+    image_name = models.CharField(max_length=100)
+    date = models.DateField()
+    slug = models.SlugField(unique=True)
+    content = models.TextField()
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL, related_name="posts", null=True)
+    tags = models.ManyToManyField(Tag, related_name="posts")
+
+    def __str__(self):
+        return self.title
+    
--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -1,96 +1,25 @@
-from datetime import date
+from django.shortcuts import render, get_object_or_404
 
-from django.shortcuts import render
+from blog.models import Post
 
-all_posts = [
-    {
-        "slug": "the-strawhats",
-        "image": "strawhats.png",
-        "author": "Gresh",
-        "date": date(2024, 6, 12),
-        "title": "The Strawhats",
-        "excerpt": "Learn what each Strawhat Pirate's dream is and what their roles are in the crew.",
-        "content": """
-                    The Strawhat Pirates consist of the captain Monkey D. Luffy, the combatant Roronoa Zoro, 
-                    the navigator Nami, the sniper Usopp, the cook Sanji, the doctor Tony Tony Chopper, the archaeologist Nico Robin,
-                    the shipwright Franky, the musician Brook, and the helmsman Jinbe. Each strawhat has their own unique abilities and dreams. 
-                    Luffy wants to become the Pirate King, Zoro wants to become the greatest swordsman, Nami wants to draw a map of the world, 
-                    Usopp wants to become a brave warrior of the sea, Sanji wants to find the All Blue, Chopper wants to become the greatest doctor,
-                    Robin wants to uncover the true history of the world, Franky wants to build a ship that can circumnavigate the world, 
-                    Brook wants to reunite with his friend Laboon, and Jinbe wants to unite the fishmen and humans. The crew all share the ambition of
-                    making Luffy the Pirate King and are willing to do anything to achieve that goal. 
-                   """
-    },
-    {
-        "slug": "gears",
-        "image": "gear2.png",
-        "author": "Gresh",
-        "date": date(2024, 6, 12),
-        "title": "Luffy's Gears",
-        "excerpt": "What are Luffy's gears and what do they do?",
-        "content": """
-                    Luffy's gears are special techniques Luffy uses to increase his power and speed. Gear Second,
-                    involves Luffy pumping his blood faster to increase his speed and power. Gear Third, involves
-                    Luffy blowing air into his bones to increase his size and strength. Gear Fourth, involves Luffy
-                    inflating his muscles and imbuing them with massive amounts of haki to significantly increase his power and speed. 
-                    Lastly, Gear Fifth, involves Luffy awakening his devil fruit, the Hito Hito no Mi, Model: Nika, which increases his
-                    physical abilities and haki, allows him to turn his surroundings into rubber, and lets him use his imagination to
-                    fight in any way he wants. Gear 2nd and 3rd were showcased in the Enies Lobby arc, Gear 4th was showcased in the Dressrosa Arc
-                    and Gear 5th was showcased in the Wano Arc.
-                   """
-    },
-    {
-        "slug": "haki",
-        "image": "haki.png",
-        "author": "Gresh",
-        "date": date(2024, 6, 12),
-        "title": "Haki Explained",
-        "excerpt": "What are the different types of Haki?",
-        "content": """
-                    Haki is the second power system in the One Piece world. Haki is divided into three types:
-                    Observation Haki, Armament Haki, and Conqueror's Haki. Observation Haki allows the user to sense the presence, strength, and emotions of others.
-                    Armament Haki allows the user to create an invisible armor around themselves, increasing their offensive and defensive capabilities.
-                    Conqueror's Haki allows the user to exert their willpower over others, causing them to faint or be intimidated. 
-                    Haki is a form of spiritual energy that flows through all living things. Armament and observation haki can be trained and developed 
-                    through rigorous training and experience, but Conqueror's Haki is a rare ability that you are born with.
-                    
-                    There are also advanced forms of each haki type. Advanced Observation Haki allows the user to see into the future. 
-                    Advanced Armament Haki allows the user to emit their haki into something else, destroying it from the inside.
-                    Advanced Conqueror's Haki allows the user to coat themselves with an invisible armor, just like Armament Haki, but it is significantly stronger,
-                    being considered not only the strongest form of Haki, but the strongest ability in the One Piece world.
-                   """
-    },
-    {
-        "slug": "roger",
-        "image": "roger.png",
-        "author": "Gresh",
-        "date": date(2024, 6, 12),
-        "title": "Gol D. Roger's Final Words",
-        "excerpt": "Roger's last words changed the world of One Piece forever, marking the beginning of the great pirate era!",
-        "content": """
-                    Narrator: Wealth, fame, power… Gold Roger, the King of the Pirates, attained this and everything else the world had to offer. And his dying words drove countless souls to the seas.
-                    Gold Roger: You want my treasure? You can have it! I left everything I gathered together in one place! Now you just have to find it!
-                    Narrator: These words lured men to the Grand Line in pursuit of dreams greater than they've ever dared to imagine! This is the time known as 'The Great Pirate Era'!
-                   """
-    },
-]
-
+all_posts = Post.objects.all().order_by("-date")
 
 # Create your views here.
 def starting_page(request):
-    sorted_posts = sorted(all_posts, key=lambda post: post["date"], reverse=True)
-    latest_posts = sorted_posts[-3:]
+    latest_posts = Post.objects.all().order_by("-date")[:3] 
     return render(request, "blog/index.html", {
         "posts": latest_posts
     })
 
 def posts(request):
+
     return render(request, "blog/all-posts.html", {
-        "all_posts": all_posts
+        "all_posts": all_posts,
     })
 
 def post_detail(request, slug):        
-    identified_post = next(post for post in all_posts if post["slug"] == slug)
+    identified_post = get_object_or_404(Post, slug=slug)
     return render(request, "blog/post-detail.html", {
-        "post": identified_post
+        "post": identified_post,
+        "post_tags": identified_post.tags.all()
     })
